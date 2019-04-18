48f51bda123173b5a03f9edb09bdef54c12b3887
Sulav Khadka <khadkasulav@gmail.com>
2019-04-18 18:57:16 -0400

    added json parsing to the joke call response

--- dadjoke.go
+++ dadjoke.go
@@ -3,33 +3,42 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
 type joke struct {
-	id     string
-	joke   string
-	status int
+	ID     string
+	Joke   string
+	Status int
 }
 
-func main() {
-
+func getDadJoke() joke {
 	client := &http.Client{}
 	req, err := http.NewRequest(http.MethodGet, "https://icanhazdadjoke.com/", nil)
 
 	if err != nil {
-		fmt.Println("Error %s\n", err)
+		fmt.Printf("Error %s\n", err)
 	}
 	req.Header.Set("Accept", "application/json")
 
 	res, getErr := client.Do(req)
 	if getErr != nil {
-		fmt.Println("Error 2 %s\n", err)
+		fmt.Printf("Error 2 %s\n", err)
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Printf("Error %s\n", err)
 	}
 
-	dadJoke := new(joke)
+	var dadJoke joke
+	json.Unmarshal([]byte(string(body)), &dadJoke)
 
-	newJoke := json.NewDecoder(res.Body).Decode(dadJoke)
+	return dadJoke
+}
 
-	fmt.Println(newJoke)
+func main() {
+	fmt.Println(getDadJoke().Joke)
 }
